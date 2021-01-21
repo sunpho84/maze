b3200f67325e8bed06703b7c34235670af6e8920
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2021-01-21 17:55:06 +0100

    Reactivated feature, renamed into dispatchable_tag

--- src/metaProgramming/feature.hpp
+++ src/metaProgramming/feature.hpp
@@ -14,11 +14,13 @@
 
 namespace maze
 {
-//   /// Provides a simple struct with the given name, to be used to catch a feature
-// #define DEFINE_FEATURE(NAME) 
-//   struct NAME		     
-//   {			     
-//   }
+  /// Provides a simple struct with the given name
+  ///
+  /// Can be used to distinguish function in place of SFINAE
+#define DECLARE_DISPATCHABLE_TAG(NAME) \
+  struct NAME			      \
+  {				      \
+  }
   
   /// Define a feature
 #define DEFINE_FEATURE(FEATURE_NAME)			\
